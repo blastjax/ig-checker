42eec49b1485926bfd5395da5f2f10c930ab0aad
mlcruz <mlcruz@unionbankph.com>
2021-07-13 09:06:36 +0800

    Separate followers and following function

--- main.py
+++ main.py
@@ -31,17 +31,36 @@ class InstaBot:
         sleep(2)
         self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
             .click()
-        following = self._get_names()
+        following = self._get_names_following()
         self.driver.find_element_by_xpath("//a[contains(@href,'/followers')]")\
             .click()
-        followers = self._get_names()
+        followers = self._get_names_followers()
         not_following_back = [
             user for user in following if user not in followers]
         for x in not_following_back:
             profiles = "https://instagram.com/" + x
             print(profiles)
 
-    def _get_names(self):
+    def _get_names_following(self):
+        sleep(2)
+        scroll_box = self.driver.find_element_by_xpath(
+            "/html/body/div[5]/div/div/div[3]")
+        last_ht, ht = 0, 1
+        while last_ht != ht:
+            last_ht = ht
+            sleep(1)
+            ht = self.driver.execute_script("""
+                arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+                return arguments[0].scrollHeight;
+                """, scroll_box)
+        links = scroll_box.find_elements_by_tag_name('a')
+        names = [name.text for name in links if name.text != '']
+        # close button
+        self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")\
+            .click()
+        return names
+
+    def _get_names_followers(self):
         sleep(2)
         scroll_box = self.driver.find_element_by_xpath(
             "/html/body/div[5]/div/div/div[2]")
