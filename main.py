e78ef88bd50068d2358ca88b5cfccdbea953e173
Luis Cruz <micahluis.cruz@sophos.com>
2021-10-27 13:30:10 +0800

    October 27, 2021 Update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys #For 2FA
 from time import sleep
 from secrets import pw
 from secrets import username
@@ -17,6 +18,8 @@ class InstaBot:
             .send_keys(pw)
         self.driver.find_element_by_xpath('//button[@type="submit"]')\
             .click()
+        sleep(10)
+        self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div[1]/div/form/div[2]/button").send_keys(Keys.ENTER)
         sleep(4)
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
             .click()
@@ -36,7 +39,8 @@ class InstaBot:
             .click()
         followers = self._get_names_followers()
         not_following_back = [
-            user for user in following if user not in followers]
+            user for user in following if user not in followers
+        ]
         for x in not_following_back:
             profiles = "https://instagram.com/" + x
             print(profiles)
@@ -44,38 +48,40 @@ class InstaBot:
     def _get_names_following(self):
         sleep(2)
         scroll_box = self.driver.find_element_by_xpath(
-            "/html/body/div[5]/div/div/div[3]")
+            "/html/body/div[6]/div/div/div[3]")
         last_ht, ht = 0, 1
         while last_ht != ht:
             last_ht = ht
             sleep(1)
-            ht = self.driver.execute_script("""
+            ht = self.driver.execute_script(
+                """
                 arguments[0].scrollTo(0, arguments[0].scrollHeight); 
                 return arguments[0].scrollHeight;
                 """, scroll_box)
         links = scroll_box.find_elements_by_tag_name('a')
         names = [name.text for name in links if name.text != '']
         # close button
-        self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")\
+        self.driver.find_element_by_xpath("/html/body/div[6]/div/div/div[1]/div/div[2]/button")\
             .click()
         return names
 
     def _get_names_followers(self):
         sleep(2)
         scroll_box = self.driver.find_element_by_xpath(
-            "/html/body/div[5]/div/div/div[2]")
+            "/html/body/div[6]/div/div/div[2]")
         last_ht, ht = 0, 1
         while last_ht != ht:
             last_ht = ht
             sleep(1)
-            ht = self.driver.execute_script("""
+            ht = self.driver.execute_script(
+                """
                 arguments[0].scrollTo(0, arguments[0].scrollHeight); 
                 return arguments[0].scrollHeight;
                 """, scroll_box)
         links = scroll_box.find_elements_by_tag_name('a')
         names = [name.text for name in links if name.text != '']
         # close button
-        self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")\
+        self.driver.find_element_by_xpath("/html/body/div[6]/div/div/div[1]/div/div[2]/button")\
             .click()
         return names
 
